2f6b2b004ded1f799b6a8a99657fd3a997bc7319
BogdanZsolt <bogdan.zsoltika@gmail.com>
2019-03-15 22:57:15 +0100

    first commit

---
+++ ubuntu-setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# use: sudo chmod +x ubuntu-setup.sh and sudo ./ubuntu-setup.sh
+# add canonical partner repo
+
+sudo add-apt-repository "deb http://archive.canonical.com/ubuntu cosmic partner"
+# update software list & upgrade 
+sudo apt update && sudo apt upgrade -y
+sudo apt install software-properties-common apt-transport-https wget
+# add chrome repo
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+#setup tlp repo
+sudo add-apt-repository ppa:linrunner/tlp
+# setup stacer System Optimizer repo
+add-apt-repository ppa:oguzhaninan/stacer
+# setup spotify repo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# setup vscode repo
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+# setup sublime repo
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-add-repository "deb https://download.sublimetext.com/ apt/stable/"
+# setup Yaru theme repo
+sudo add-apt-repository ppa:communitheme/ppa
+# update repo list
+sudo apt update
+#--------------------------------------------------
+#install software
+sudo apt -y install $(cat ubuntu.packages)
+#SNAP install
+sudo snap install slack --classic
+sudo snap install gitkraken
+sudo snap install mailspring
+sudo snap install caprine
+# Remove Amazon Launcher
+sudo apt -y purge ubuntu-web-launchers 
+# Improve Ubuntu Battery 
+sudo tlp start
+sudo apt remove -y $(cat ubuntu.removes)
